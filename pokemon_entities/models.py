243a2cd91d4cae3fe52b1ce0f4d60b6876a7caeb
ruk228 <icecreamcrusher21@gmail.com>
2022-08-11 01:38:15 +0300

    first commit

---
+++ pokemon_entities/models.py
@@ -0,0 +1,31 @@
+from email.base64mime import header_length
+from django.db import models  # noqa F401
+from django.forms.models import model_to_dict
+from django.utils.timezone import localtime
+# your models here
+
+class Pokemon(models.Model):
+    name = models.TextField(blank=True, max_length=25, verbose_name='Русское имя')
+    name_en = models.TextField(blank=True, max_length=25, verbose_name='Английское имя')
+    name_jp = models.TextField(blank=True, max_length=25, verbose_name='Японское имя')
+    image = models.ImageField(blank=True, verbose_name='Название картинки')
+    description = models.TextField(blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return str(self.name)
+
+
+class PokemonEntity(models.Model):
+    pokemon_description = models.ForeignKey(Pokemon, null=True, verbose_name='Покемон', on_delete=models.CASCADE, related_name='pokemon_entities')
+    lat = models.FloatField(null=True, verbose_name='Широта')
+    low = models.FloatField(null=True, verbose_name='Долгота')
+    appeared_at = models.DateTimeField(null=True, verbose_name='Время включения')
+    disappeared_at = models.DateTimeField(null=True, verbose_name='Время отключения')
+    level = models.IntegerField(null=True, verbose_name='Уровень')
+    health = models.IntegerField(null=True, verbose_name='Здоровье')
+    strength = models.IntegerField(null=True, verbose_name='Прочность')
+    defencer = models.IntegerField(null=True, verbose_name='Защита')
+    stamina = models.IntegerField(null=True, verbose_name='стамина')
+
+    def __str__(self):
+        return str(self.pokemon_description)
---
+++ pokemon_entities/views.py
@@ -0,0 +1,118 @@
+import folium
+
+from .models import PokemonEntity, Pokemon
+from django.http import HttpResponseNotFound
+from django.shortcuts import render
+from requests import request
+from django.utils.timezone import localtime
+
+
+MOSCOW_CENTER = [55.751244, 37.618423]
+DEFAULT_IMAGE_URL = (
+    'https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision'
+    '/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832'
+    '&fill=transparent'
+)
+
+
+def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
+    icon = folium.features.CustomIcon(
+        image_url,
+        icon_size=(50, 50),
+    )
+    folium.Marker(
+        [lat, lon],
+        icon=icon,
+    ).add_to(folium_map)
+
+
+def show_all_pokemons(request):
+    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)    
+    pokemons = Pokemon.objects.all()
+    pokemons_on_page = []
+
+    for pokemon in pokemons:
+        pokemon_entity = pokemon.pokemon_entities.filter(disappeared_at__gte=localtime(), appeared_at__lte=localtime())[0]
+        img_url = request.build_absolute_uri('media/{}'.format(pokemon.image))
+
+        pokemons_on_page.append({
+            'pokemon_id':  pokemon.id,
+            'img_url': img_url,
+            'title_ru': pokemon.name
+        })
+
+        add_pokemon(
+            folium_map, pokemon_entity.lat,
+            pokemon_entity.low,
+            img_url
+        )
+
+    return render(request, 'mainpage.html', context={
+        'map': folium_map._repr_html_(),
+        'pokemons': pokemons_on_page,
+    })
+
+
+def show_pokemon(request, pokemon_id):
+    pokemons = []
+    pokemon = Pokemon.objects.all()
+    pokemon_entity = pokemon.get(id=pokemon_id).pokemon_entities.filter(disappeared_at__gte=localtime(), appeared_at__lte=localtime())[0]
+    
+    try:
+        previous_pokemon = pokemon.get(id=int(pokemon_id)-1)
+        previous_evolution = {
+            "title_ru": previous_pokemon.name,
+            "pokemon_id": previous_pokemon.id,
+            "img_url": request.build_absolute_uri('../../media/{}'.format(previous_pokemon.image))
+        }
+    except:
+        previous_evolution = {}
+
+    try:
+        next_pokemon = pokemon.get(id=int(pokemon_id)+1)
+        next_evolution = {
+            "title_ru": next_pokemon.name,
+            "pokemon_id": next_pokemon.id,
+            "img_url": request.build_absolute_uri('../../media/{}'.format(next_pokemon.image))
+        }
+    except:
+        next_evolution = {}
+
+    pokemons.append(
+        {
+            'pokemon_id': pokemon_entity.id,
+            'title_ru': pokemon_entity.pokemon_description.name,
+            'title_en': pokemon_entity.pokemon_description.name_en,
+            'title_jp': pokemon_entity.pokemon_description.name_jp,
+            'description': pokemon_entity.pokemon_description.description,
+            'img_url': request.build_absolute_uri('../../media/{}'.format(pokemon_entity.pokemon_description.image)),
+            'entities': [
+                {
+                    'level': pokemon_entity.level,
+                    'lat': pokemon_entity.lat,
+                    'lon': pokemon_entity.low
+                },
+                ],
+            "next_evolution": next_evolution,
+            'previous_evolution': previous_evolution
+        },
+    )
+
+    for pokemon in pokemons:
+        if pokemon['pokemon_id'] == int(pokemon_id):
+            requested_pokemon = pokemon
+            break
+    else:
+        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+
+    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
+    for pokemon_entity in requested_pokemon['entities']:
+        add_pokemon(
+            folium_map, pokemon_entity['lat'],
+            pokemon_entity['lon'],
+            pokemon['img_url']
+        )
+
+    return render(request, 'pokemon.html', context={
+        'map': folium_map._repr_html_(), 'pokemon': pokemon
+    })
